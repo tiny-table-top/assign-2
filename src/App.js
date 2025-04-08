b7bd7f716d5ccc80364edbeef8d4d0f9c4b8a87c
Anthony <a.half915@mybvc.ca>
2025-04-07 19:02:35 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My name is Anthony and I like this atom looking thingy!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
